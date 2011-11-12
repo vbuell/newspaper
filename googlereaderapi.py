cdff234fa9c372175655c4eb2dc9a802c0ae133d
vbuell <vbuell@5da9f3c4-b4a7-cd5e-3b64-2c8283a59a09>
2011-11-12 17:45:45 +0000

    Initial import

---
+++ googlereaderapi.py
@@ -0,0 +1,278 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+#
+#
+# googlereaderapi.py
+# 
+# A python wrapper for the Google Reader
+#
+# Copyright (C) 2011 Lorenzo Carbonell
+# lorenzo.carbonell.cerezo@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = 'Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>'
+__date__ = '$13/03/2011'
+__copyright__ = 'Copyright (c) 2011 Lorenzo Carbonell'
+__license__ = 'GPLV3'
+__url__ = 'http://www.atareao.es'
+
+import simplejson
+import urllib
+import urllib2
+from lxml.html.clean import clean_html
+import time
+import logging
+
+class Article(object):
+    def __init__(self, elemento):
+        self.is_read = False
+        self.is_marked_as_unread = False
+        self.is_liked = False
+        self.is_shared = False
+        self.is_starred = False
+        self.is_browsed = False
+        self.is_emailed = False
+        self.is_twitter = False
+        self.is_readitlater = False
+        #
+        self.crawlTimeMsec = self._get(elemento, 'crawlTimeMsec')
+        self.id = self._get(elemento, 'id')
+        self.categories = self._get(elemento, 'categories')
+        self.title = self._get(elemento, 'title')
+        self.alternate = self._get(elemento, 'alternate')
+        if len(self.alternate) > 0:
+            self.alternate = self.alternate[0]
+        self.published = self._get(elemento, 'published')
+        self.updated = self._get(elemento, 'updated')
+        self.summary = self._get(elemento, 'summary')
+        if self.summary is not None:
+            self.summary = clean_html('<div>%s</div>' % (self.summary['content']))
+        self.content = self._get(elemento, 'content')
+        if self.content is not None:
+            self.content = clean_html('<div>%s</div>' % (self.content['content']))
+        self.author = self._get(elemento, 'author')
+        self.likingUsers = self._get(elemento, 'likingUsers')
+        self.comments = self._get(elemento, 'comments')
+        self.annotations = self._get(elemento, 'annotations')
+        self.origin = self._get(elemento, 'origin')
+
+    # origin['streamId']
+    # origin['title']
+    # origin['htmlUrl']
+
+    def _get(self, elemento, key):
+        if key in elemento:
+            return elemento[key]
+        return None
+
+
+class GoogleReader(object):
+    def __init__(self, username, password):
+        self.client = 'read-me/0.1.0'
+        self.username = username
+        self.email = username
+        self.password = password
+        self.noticias = []
+        self.get_header()
+
+    def get_header(self):
+        # Authenticate to obtain Auth
+        auth_url = 'https://www.google.com/accounts/ClientLogin'
+        auth_req_data = urllib.urlencode({
+            'Email': self.username,
+            'Passwd': self.password,
+            'service': 'reader',
+            'source': self.client
+        })
+        auth_req = urllib2.Request(auth_url, data=auth_req_data)
+        auth_resp = urllib2.urlopen(auth_req)
+        auth_resp_content = auth_resp.read()
+        auth_resp_dict = dict(x.split('=') for x in auth_resp_content.split('\n') if x)
+        AUTH = auth_resp_dict['Auth']
+        # Create a cookie in the header using the Auth
+        self.header = {'User-Agent': 'Magic Browser', 'Authorization': 'GoogleLogin auth=%s' % AUTH}
+        self.get_token()
+
+    def get_token(self):
+        reader_url = 'https://www.google.com/reader/api/0/token'
+        reader_req = urllib2.Request(reader_url, None, self.header)
+        answer = urllib2.urlopen(reader_req)
+        self.token = answer.read()
+        return self.token
+
+
+    def _mark_as(self, action, entryid, mark):
+        if mark:
+            option = 'a'
+        else:
+            option = 'r'
+        post_data = urllib.urlencode({'i': entryid,
+                                      option: 'user/-/state/com.google/%s' % (action),
+                                      'ac': 'edit',
+                                      'T': self.token})
+        url = 'http://www.google.com/reader/api/0/edit-tag'
+        request = urllib2.Request(url, post_data, self.header)
+        try:
+            f = urllib2.urlopen(request)
+        except Exception, e:
+            logging.exception("urlopen error:")
+            # Authorization finished
+            self.get_header()
+            url = 'http://www.google.com/reader/api/0/edit-tag'
+            request = urllib2.Request(url, post_data, self.header)
+            f = urllib2.urlopen(request)
+        result = f.read()
+        if result == 'OK':
+            return True
+        else:
+            self.get_token()
+            url = 'http://www.google.com/reader/api/0/edit-tag'
+            request = urllib2.Request(url, post_data, self.header)
+            f = urllib2.urlopen(request)
+            result = f.read()
+            if result == 'OK':
+                return True
+        self.get_token()
+        return False
+
+    def mark_as_starred(self, entryid, is_marked):
+        return self._mark_as('starred', entryid, is_marked)
+
+    def mark_as_kept_unread(self, entryid, is_marked):
+        return self._mark_as('tracking-kept-unread', entryid, is_marked)
+
+    def mark_as_shared(self, entryid, is_marked):
+        return self._mark_as('broadcast', entryid, is_marked)
+
+    def mark_as_browsed(self, entryid, is_marked):
+        return self._mark_as('tracking-item-link-used', entryid, is_marked)
+
+    def mark_as_emailed(self, entryid, is_marked):
+        return self._mark_as('tracking-emailed', entryid, is_marked)
+
+    def mark_as_like(self, entryid, is_marked):
+        return self._mark_as('like', entryid, is_marked)
+
+    def mark_as_read(self, entryid, is_marked):
+        return self._mark_as('read', entryid, is_marked)
+
+    def get_shared_list(self, number_of_items=20, from_past_to_now=True):
+        shared = []
+        url = 'https://www.google.com/reader/api/0/stream/contents/user/-/state/com.google/broadcast%s'
+        if from_past_to_now:
+            r = 'o'
+        else:
+            r = 'd'
+        get_data = urllib.urlencode({'n': str(number_of_items),
+                                     'ck': int(time.time()),
+                                     'r': r,
+                                     'client': self.client})
+        # Doing GET
+        reader_url = url % (get_data)
+        request = urllib2.Request(reader_url, None, self.header)
+        f = urllib2.urlopen(request)
+        if f:
+            return simplejson.loads(f.read())
+        return None
+
+        '''
+      def mark_all_as_read(self, SID):
+         url    = 'http://www.google.com/reader/api/0/mark-all-as-read?client=contact:%s" % (self.login)
+         data   = { 's': 'user/06091295523448519803/state/com.google/reading-list',
+                    't': 'All%20items',
+                    'T': self._get_token(SID),
+                    'ts': int(time.time())
+                  }
+         result = self.cm.post(url, self._header(), data, self._cookie(SID))
+         if result: pass
+         else: print 'Error: mark_all_as_read'
+         '''
+
+    def get_reading_list(self, number_of_items=20, from_past_to_now=True, continuation=None):
+        shared = []
+        url = 'https://www.google.com/reader/api/0/stream/contents/user/-/state/com.google/reading-list?%s'
+        if from_past_to_now:
+            r = 'o'
+        else:
+            r = 'd'
+        map = {'n': str(number_of_items),
+                                     'ck': int(time.time()),
+                                     'r': r,
+                                     'client': self.client,
+                                     'xt': 'user/-/state/com.google/read'}
+        if continuation:
+            map['c'] = continuation
+        get_data = urllib.urlencode(map)
+        # Doing GET
+        reader_url = url % (get_data)
+        request = urllib2.Request(reader_url, None, self.header)
+        f = urllib2.urlopen(request)
+        if f:
+            return simplejson.loads(f.read())
+        return None
+
+    def get_entries(self, feed_id, number_of_items=20, from_past_to_now=True, continuation=None):
+        url =      'http://www.google.com/reader/api/0/stream/contents/%s?%s'#ot=1317981600&r=n&xt=user%2F15348108177535720089%2Fstate%2Fcom.google%2Fread&likes=false&comments=false&n=20&ck=1320576432916&client=scroll
+#        url = 'https://www.google.com/reader/api/0/stream/contents/user/-/label/%s?%s'
+        if from_past_to_now:
+            r = 'o'
+        else:
+            r = 'd'
+        map = {'n': str(number_of_items),
+                                     'ck': int(time.time()),
+                                     'r': r,
+                                     'client': self.client,
+                                     'xt': 'user/-/state/com.google/read'}
+        if continuation:
+            map['c'] = continuation
+        get_data = urllib.urlencode(map)
+        # Doing GET
+        reader_url = url % (urllib.quote(feed_id), get_data)
+        print "^^^^^^^^^^^",reader_url
+        request = urllib2.Request(reader_url, None, self.header)
+        f = urllib2.urlopen(request)
+        if f:
+            return simplejson.loads(f.read())
+        return None
+
+    def get_unread_count(self):
+        reader_base_url = 'http://www.google.com/reader/api/0/unread-count?%s'
+        reader_req_data = urllib.urlencode({'all': 'true',
+                                            'output': 'json',
+                                            'ck': int(time.time()),
+                                            'client': self.client})
+        reader_url = reader_base_url % (reader_req_data)
+        reader_req = urllib2.Request(reader_url, None, self.header)
+        reader_resp = urllib2.urlopen(reader_req)
+        if reader_resp:
+            j = simplejson.loads(reader_resp.read())
+            count = ([c['count'] for c in j['unreadcounts'] if c['id'].endswith('/state/com.google/reading-list')] or [0])[0]
+            return count, j
+        return None
+
+    def get_subscriptions(self):
+        url = 'http://www.google.com/reader/api/0/subscription/list?%s'
+        get_data = urllib.urlencode({'ck': int(time.time()),
+                                     'output': 'json',
+                                     'client': self.client})
+        # Doing GET
+        reader_url = url % get_data
+        print reader_url
+        request = urllib2.Request(reader_url, None, self.header)
+        f = urllib2.urlopen(request)
+        if f:
+            obj = simplejson.loads(f.read())
+            return obj
+        return None
---
+++ news_greader.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import datetime
+import logging
+import urllib2
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gconf
+import gobject
+import webkit
+import pango
+import webbrowser
+
+from googlereaderapi import GoogleReader
+
+
+class Preferences:
+    """
+    Application preferences.
+    """
+    ROOT_DIR = '/apps/news'
+
+    KEY_WINDOW_WIDTH = ROOT_DIR + '/ui_width'
+    KEY_WINDOW_HEIGHT = ROOT_DIR + '/ui_height'
+    KEY_GREADER_LOGIN = ROOT_DIR + '/greader_login'
+    KEY_GREADER_PASS = ROOT_DIR + '/greader_pass'
+
+    windowWidth = 700
+    windowHeight = 800
+    windowX = 200
+    windowY = 200
+    greader_login = None
+    greader_pass = None
+
+    @staticmethod
+    def load():
+        # GConf stuff
+        gconf_client = gconf.client_get_default()
+        Preferences.gconf_client = gconf_client
+        gconf_client.add_dir(Preferences.ROOT_DIR, gconf.CLIENT_PRELOAD_NONE)
+
+        try:
+            gconf_value = int(gconf_client.get_string(Preferences.KEY_WINDOW_WIDTH))
+            if gconf_value:
+                Preferences.windowWidth = gconf_value
+            gconf_value = int(gconf_client.get_string(Preferences.KEY_WINDOW_HEIGHT))
+            if gconf_value:
+                Preferences.windowHeight = gconf_value
+            gconf_value = gconf_client.get_string(Preferences.KEY_GREADER_LOGIN)
+            if gconf_value:
+                Preferences.greader_login = gconf_value
+            gconf_value = gconf_client.get_string(Preferences.KEY_GREADER_PASS)
+            if gconf_value:
+                Preferences.greader_pass = gconf_value
+
+            logging.debug("Loaded preferences from gconf: " + str(Preferences.__dict__))
+        except Exception, e:
+            logging.exception(e)
+
+    @staticmethod
+    def save():
+        Preferences.gconf_client.set_string(Preferences.KEY_WINDOW_WIDTH, str(Preferences.windowWidth))
+        Preferences.gconf_client.set_string(Preferences.KEY_WINDOW_HEIGHT, str(Preferences.windowHeight))
+        Preferences.gconf_client.set_string(Preferences.KEY_GREADER_LOGIN, str(Preferences.greader_login))
+        Preferences.gconf_client.set_string(Preferences.KEY_GREADER_PASS, str(Preferences.greader_pass))
+
+        logging.debug("Preferences saved.")
+
+class News:
+
+    def __init__(self):
+        Preferences.load()
+
+        self.create_main_window()
+
+        if not Preferences.greader_login or not Preferences.greader_pass:
+            self.show_login(False)
+        else:
+            self.init_greader()
+
+    def init_greader(self):
+        try:
+            logging.debug("Logging in as: " + str(Preferences.greader_login))
+            self.google_reader = GoogleReader(Preferences.greader_login, Preferences.greader_pass)
+            self.populate_feeds()
+            self.title_changed(None, None, "tag#default")
+        except urllib2.HTTPError:
+            logging.exception("Forbidden")
+            self.show_login(True)
+
+    def delete_event(self, event, data=None):
+        """Closes the app through window manager signal"""
+        Preferences.save()
+        gtk.main_quit()
+        return False
+
+    def title_changed(self, widget, frame, title):
+        if title is None:
+            return
+        print "COMMAND", title
+        command = title[:title.find("#")]
+        rest = title[title.find("#")+1:]
+        print "Command form UI: " + command + rest
+        logging.debug("Command form UI: " + command + rest)
+        if title.startswith("markasread#"):
+#            print id
+            try:
+                self.google_reader.mark_as_read(rest, True)
+            except:
+                logging.exception("Can't mark as read")
+                self.web_send('unslide("'+rest+'")')
+        elif title.startswith("showbrowser#"):
+#            print id
+            webbrowser.open(rest, new = 1)
+        elif title.startswith("tag#"):
+            if '#' in rest:
+                chunks = rest.split('#')
+                html_feed_page = self.return_entries_of_feed(chunks[0], continuation=chunks[1])
+            else:
+                html_feed_page = self.return_entries_of_feed(rest)
+            self.webview.load_string(html_feed_page, "text/html", "utf-8", "valid_link")
+        elif title.startswith("login#"):
+            Preferences.greader_login = rest[:rest.find("#")]
+            Preferences.greader_pass = rest[rest.find("#")+1:]
+            self.init_greader()
+        elif title.startswith("logout#"):
+            Preferences.greader_login = None
+            Preferences.greader_pass = None
+            self.show_login(False)
+
+    def web_send(self, msg):
+        if msg:
+            print '<<<', msg
+            asynchronous_gtk_message(self.webview.execute_script)(msg)
+
+    def show_about(self, action):
+        """Shows the about message dialog"""
+        about = gtk.AboutDialog()
+        about.set_transient_for(self.window)
+        about.set_program_name("News!")
+        about.set_version("0.90")
+        about.set_comments("News! :: Minimalistic Google Reader frontend")
+        about.set_copyright("(c) 2011 Volodymyr Buell")
+        about.set_website("http://code.google.com/p/newspaper/")
+        about.run()
+        about.destroy()
+
+    def create_ui(self, window):
+        """Creates the menubar"""
+        ui_string = """<ui>
+               <menubar name='Menubar'>
+                <menu action='HelpMenu'>
+                 <menuitem action='About'/>
+                </menu>
+               </menubar>
+              </ui>"""
+
+        ag = gtk.ActionGroup('WindowActions')
+        actions = [
+                ('HelpMenu', None, '_Help'),
+                ('About', gtk.STOCK_ABOUT, '_About', None, 'About', self.show_about),
+                ]
+        ag.add_actions(actions)
+        self.ui = gtk.UIManager()
+        self.ui.insert_action_group(ag, 0)
+        self.ui.add_ui_from_string(ui_string)
+        self.window.add_accel_group(self.ui.get_accel_group())
+
+    def create_main_window(self):
+        """Creates the main window with all it's widgets"""
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("News!")
+        self.window.connect("delete_event", self.delete_event)
+        self.window.set_border_width(0)
+        self.window.move(Preferences.windowX, Preferences.windowY)
+        self.window.resize(Preferences.windowWidth, Preferences.windowHeight)
+
+        self.vbox = gtk.VBox(False, 0)
+        self.create_ui(self.window)
+        self.vbox.pack_start(self.ui.get_widget('/Menubar'), False, True, 0)
+        self.hpaned = gtk.HPaned()
+
+        self.scrolled_window3 = gtk.ScrolledWindow()
+        self.scrolled_window3.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.webview = webkit.WebView()
+        self.webview.set_full_content_zoom(True)
+        self.webview.connect_after("populate-popup", self.create_webview_popup)
+        # Open in new browser handler (this intercepts all requests)
+        self.webview.connect("hovering-over-link", self.hover_link)
+        self.webview.connect('title-changed', self.title_changed)
+
+        self.scrolled_window3.add(self.webview)
+
+        self.hpaned.add2(self.scrolled_window3)
+        self.vbox.pack_start(self.hpaned, True, True, 0)
+
+        self.hbox2 = gtk.HBox(homogeneous=False, spacing=0)
+        self.statusbar = gtk.Label("")
+        self.statusbar.set_justify(gtk.JUSTIFY_LEFT)
+        self.statusbar.set_ellipsize(pango.ELLIPSIZE_END)
+        self.statusbar.set_alignment(xalign=0.01, yalign=0)
+        self.hbox2.pack_start(self.statusbar, expand=True, fill=True, padding=0)
+        self.vbox.pack_start(self.hbox2, False, False, 0)
+
+        self.window.add(self.vbox)
+
+        self.window.show_all()
+
+    def create_webview_popup(self, view, menu):
+        """Creates the webview (browser item) popup menu."""
+        zoom_in = gtk.ImageMenuItem(gtk.STOCK_ZOOM_IN)
+        zoom_in.connect('activate', self.zoom_in_cb, view)
+        menu.append(zoom_in)
+        zoom_out = gtk.ImageMenuItem(gtk.STOCK_ZOOM_OUT)
+        zoom_out.connect('activate', self.zoom_out_cb, view)
+        menu.append(zoom_out)
+        zoom_hundred = gtk.ImageMenuItem(gtk.STOCK_ZOOM_100)
+        zoom_hundred.connect('activate', self.zoom_hundred_cb, view)
+        menu.append(zoom_hundred)
+        menu.show_all()
+        return False
+
+    def zoom_in_cb(self, menu_item, web_view):
+        """Zoom into the page"""
+        web_view.zoom_in()
+
+    def zoom_out_cb(self, menu_item, web_view):
+        """Zoom out of the page"""
+        web_view.zoom_out()
+
+    def zoom_hundred_cb(self, menu_item, web_view):
+        """Zoom 100%"""
+        if not (web_view.get_zoom_level() == 1.0):
+            web_view.set_zoom_level(1.0)
+
+    def hover_link(self, title, uri, data=None):
+        """Shows hovered link in Statusbar."""
+        if data is not None:
+            self.statusbar.set_text(data)
+        else:
+            self.statusbar.set_text("")
+
+    def populate_feeds(self):
+        counts = {}
+        num, obj = self.google_reader.get_unread_count()
+        for cntobj in obj['unreadcounts']:
+            counts[cntobj['id']] = cntobj['count']
+
+        subscriptions = self.google_reader.get_subscriptions()
+        self.subscriptions = subscriptions
+
+        # Get folders and create a map
+        DEFAULT_LABEL = 'Others'
+        categories = {}
+        categories_id = {}
+        for subscription in subscriptions['subscriptions']:
+            cats = subscription['categories']
+            if not cats:
+                if DEFAULT_LABEL not in categories:
+                    categories[DEFAULT_LABEL] = []
+                categories[DEFAULT_LABEL].append(subscription)
+            for cat in cats:
+                categories_id[cat['label']] = cat['id']
+                if cat['label'] not in categories:
+                    categories[cat['label']] = []
+                categories[cat['label']].append(subscription)
+
+        self.html_tags = ""
+        for category_name in categories:
+            if category_name in categories_id:
+                self.html_tags += '<p><span class="tag" style="font-weight: bold; " href="'+categories_id[category_name]+'">' + category_name + ':</span> '
+            else:
+                self.html_tags += '<p><b>' + category_name + ':</b> '
+
+            for feed in categories[category_name]:
+                if feed['id'] not in counts:
+                    feed_label = feed['title']
+                    font_style = 'normal'
+                    # Do not add feed if empty
+                else:
+                    feed_label = feed['title'] + ' [' + str(counts[feed['id']]) + ']'
+                    font_style = 'bold'
+                    self.html_tags += '<span class="tag" href="'+feed['id']+'">' + feed_label + "&nbsp;</span>"
+
+    def return_entries_of_feed(self, id_feed, continuation=None):
+        """Obtains the entries of the selected feed"""
+        if id_feed == "default":
+            entries = self.google_reader.get_reading_list(from_past_to_now=False, continuation=continuation)
+        else:
+            entries = self.google_reader.get_entries(id_feed, from_past_to_now=False, continuation=continuation)
+
+        f = open('./web/template.html', 'r')
+        html = f.read()
+
+        html_entries = ""
+        for row in entries['items']:
+            now = datetime.datetime.now().strftime("%Y-%m-%d")
+            content = "Unexpected..."
+            if 'content' in row:
+                content = row['content']['content']
+            elif 'summary' in row:
+                content = row['summary']['content']
+            else:
+                content = row['title']
+
+            if 'origin' in row and 'title' in row['origin']:
+                feed_name = row['origin']['title']
+            else:
+                feed_name = '&nbsp;'
+            if 'alternate' in row and row['alternate'] and 'href' in row['alternate'][0]:
+                url = row['alternate'][0]['href']
+                html_entries = html_entries + \
+                               '<div class="article-section" name="'+str(row['id'])+'">' + \
+                               '<div class="article-title" href="'+url+'">' + row['title'] + '<span>'+feed_name+'</span></div><div class="article">' + content + '</div><br></div>'
+            else:
+                html_entries = html_entries + \
+                               '<div class="article-section" name="'+str(row['id'])+'">' + \
+                               '<div class="article-title">' + row['title'] + '</div><div class="article">' + content + '</div><br></div>'
+
+        if 'continuation' in entries:
+            continuation = entries['continuation']
+            html_continuation = '<span class="tag" href="'+id_feed + '#'+continuation+'">Next...</span>'
+        else:
+            html_continuation = ''
+
+        html = html.replace("%entries%", html_entries)
+        html = html.replace("%tags%", self.html_tags)
+        html = html.replace("%feed_name%", id_feed)
+        html = html.replace("%next%", html_continuation)
+
+        return html
+
+    def show_login(self, error):
+        f = open('./web/login.html', 'r')
+        html = f.read()
+        if error:
+            html = html.replace("%error%", '<div id="login_error" class="hidden"><strong>ERROR</strong>: Invalid email or username.<br /></div>')
+        else:
+            html = html.replace("%error%", '')
+        self.webview.load_string(html, "text/html", "utf-8", "valid_link")
+
+
+def asynchronous_gtk_message(fun):
+
+    def worker((function, args, kwargs)):
+        apply(function, args, kwargs)
+
+    def fun2(*args, **kwargs):
+        gobject.idle_add(worker, (fun, args, kwargs))
+
+    return fun2
+
+
+if __name__ == "__main__":
+
+    FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+    logging.info("News! started...")
+
+    news = News()
+    gtk.main()
